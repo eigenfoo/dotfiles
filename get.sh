bb0b3bb9200c4030ef547d8502b201c520090162
George Ho <git@eigenfoo.xyz>
2019-01-16 15:23:18 -0500

    Create get.sh

---
+++ get.sh
@@ -0,0 +1,8 @@
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/.bash_logout
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/.bash_profile
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/.bashrc
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/.gitconfig
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/.hushlogin
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/.profile
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/.vimrc
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/.zshrc
