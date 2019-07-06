df002a0a5ea755bd251230009af264240961c8c9
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-07-06 14:29:14 +0800

    MAINT: update install script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
-cd
+#!/bin/sh
+cd ~
 rm .bash_logout .bash_profile .bashrc .gitconfig .hushlogin .profile .vimrc .zshrc
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/.bash_logout
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/.bash_profile
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/.bashrc
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/.gitconfig
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/.hushlogin
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/.profile
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/.vimrc
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/.zshrc
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.bash_logout
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.bash_profile
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.bashrc
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.profile
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.hushlogin
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/git/.gitconfig
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/vim/.vimrc
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/zsh/.zshrc
 exit 0
