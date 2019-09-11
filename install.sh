62e0fad68e1c14fe4d19c32931605d3dd4377609
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-09-11 14:00:40 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 cd ~
-rm .bash_logout .bash_profile .bashrc .gitconfig .hushlogin .profile .vimrc .zshrc
+rm .bash_logout .bash_profile .bashrc .bash_aliases .gitconfig .hushlogin .profile .vimrc .zshrc
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.bash_logout
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.bash_profile
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.bashrc
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.bash_aliases
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.profile
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.hushlogin
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/git/.gitconfig
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/vim/.vimrc
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/zsh/.zshrc
+source .bash_logout .bash_profile .bashrc .bash_aliases .gitconfig .hushlogin .profile .vimrc .zshrc
 exit 0
