2b2944baaf09836a264f3dcc34a0d992bc1677bb
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-10-27 22:41:41 -0400

    MAINT: move git wget further down

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.bashrc
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.bash_aliases
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.profile
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.hushlogin
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/git/.gitconfig
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/vim/.vimrc
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/zsh/.zshrc
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/git/.gitconfig
 source .bash_logout .bash_profile .bashrc .bash_aliases .gitconfig .hushlogin .profile .vimrc .zshrc
 exit 0
