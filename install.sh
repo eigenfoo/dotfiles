5b4e9cb91759de95baf26354ba639984921015d6
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-11-03 12:15:24 -0500

    MAINT: update install script to include tmux config

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ~
-rm -f .bash_logout .bash_profile .bashrc .bash_aliases .gitconfig .hushlogin .profile .vimrc .zshrc
+rm -f .bash_logout .bash_profile .bashrc .bash_aliases .profile .hushlogin .vimrc .zshrc .tmux.conf .gitconfig
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.bash_logout
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.bash_profile
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.bashrc
@@ -9,6 +9,7 @@ wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.profile
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.hushlogin
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/vim/.vimrc
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/zsh/.zshrc
+wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/tmux/.tmux.conf
 wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/git/.gitconfig
-source .bash_logout .bash_profile .bashrc .bash_aliases .gitconfig .hushlogin .profile .vimrc .zshrc
+source .bash_logout .bash_profile .bashrc .bash_aliases .profile .hushlogin .vimrc .zshrc .tmux.conf .gitconfig
 exit 0
