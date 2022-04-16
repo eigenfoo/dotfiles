b62b9e2a54683a0f9b56c2c8b373c675d84d1563
George Ho <19851673+eigenfoo@users.noreply.github.com>
2022-04-16 18:56:41 -0400

    Move dotfiles to separate dir

--- install.sh
+++ install.sh
@@ -3,25 +3,25 @@
 # Install dotfiles.
 cd ~
 rm -f .bash_logout .bash_profile .bashrc .bash_aliases .profile .hushlogin .vimrc .zshrc .tmux.conf .gitconfig
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.bash_logout
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.bash_profile
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.bashrc
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.bash_aliases
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.profile
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bash/.hushlogin
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/vim/.vimrc
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/zsh/.zshrc
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/tmux/.tmux.conf
+wget https://raw.githubusercontent.com/eigenfoo/georgelib/master/dotfiles/.bash_logout
+wget https://raw.githubusercontent.com/eigenfoo/georgelib/master/dotfiles/.bash_profile
+wget https://raw.githubusercontent.com/eigenfoo/georgelib/master/dotfiles/.bashrc
+wget https://raw.githubusercontent.com/eigenfoo/georgelib/master/dotfiles/.bash_aliases
+wget https://raw.githubusercontent.com/eigenfoo/georgelib/master/dotfiles/.profile
+wget https://raw.githubusercontent.com/eigenfoo/georgelib/master/dotfiles/.hushlogin
+wget https://raw.githubusercontent.com/eigenfoo/georgelib/master/dotfiles/.vimrc
+wget https://raw.githubusercontent.com/eigenfoo/georgelib/master/dotfiles/.zshrc
+wget https://raw.githubusercontent.com/eigenfoo/georgelib/master/dotfiles/.tmux.conf
 # I usually have a .gitconfig for work.
-# wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/git/.gitconfig
+# wget https://raw.githubusercontent.com/eigenfoo/georgelib/master/dotfiles/.gitconfig
 source .bash_logout .bash_profile .bashrc .bash_aliases .profile .hushlogin .vimrc .zshrc .tmux.conf # .gitconfig
 
 # Install bash scripts. Assumes that ~/bin/ is on the $PATH.
 mkdir -p ~/bin/
 cd ~/bin/
 rm -f extract ff
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bin/extract
-wget https://raw.githubusercontent.com/eigenfoo/dotfiles/master/bin/ff
+wget https://raw.githubusercontent.com/eigenfoo/georgelib/master/bin/extract
+wget https://raw.githubusercontent.com/eigenfoo/georgelib/master/bin/ff
 chmod +x extract ff
 
 # Install Vim plugins.
